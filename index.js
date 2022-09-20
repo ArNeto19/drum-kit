a6fdba662ddc82b25bef6751761021e32fc94e62
arneto19 <armandopdneto@gmail.com>
2022-09-19 22:36:11 -0300

    General refactoring improvements using jQuery.

--- index.js
+++ index.js
@@ -1,65 +1,68 @@
-var numberOfDrumButtons = document.querySelectorAll('.drum').length;
+//jshint esversion:6
 
-for (var i = 0; i < numberOfDrumButtons; i++) {
-  document.querySelectorAll('.drum')[i].addEventListener('click', function() {
-    var buttonInnerHTML = this.innerHTML;
+const drumButtons = $('.drum');
+const numberOfDrumButtons = drumButtons.length;
+
+for (let i = 0; i < numberOfDrumButtons; i++) {
+  drumButtons.eq(i).on("click", (event) => {
+    let buttonInnerHTML = this.event.target.innerHTML;
     makeSound(buttonInnerHTML);
     buttonAnimation(buttonInnerHTML);
-  })
+  });
 }
 
-document.addEventListener('keydown', function(event) {
-  makeSound(event.key)
-  buttonAnimation(event.key)
-})
+$(document).keydown((event) => {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
 
 function makeSound(key) {
 
   switch (key) {
     case key = 'w':
-      let tom1 = new Audio('sounds/tom-1.mp3')
+      let tom1 = new Audio('sounds/tom-1.mp3');
       tom1.play();
       break;
     case key = 'a':
-      let tom2 = new Audio('sounds/tom-2.mp3')
+      let tom2 = new Audio('sounds/tom-2.mp3');
       tom2.play();
       break;
     case key = 's':
-      let tom3 = new Audio('sounds/tom-3.mp3')
+      let tom3 = new Audio('sounds/tom-3.mp3');
       tom3.play();
       break;
     case key = 'd':
-      let tom4 = new Audio('sounds/tom-4.mp3')
+      let tom4 = new Audio('sounds/tom-4.mp3');
       tom4.play();
       break;
     case key = 'j':
-      let snare = new Audio('sounds/snare.mp3')
+      let snare = new Audio('sounds/snare.mp3');
       snare.play();
       break;
     case key = 'k':
-      let crash = new Audio('sounds/crash.mp3')
+      let crash = new Audio('sounds/crash.mp3');
       crash.play();
       break;
     case key = 'l':
-      let kick = new Audio('sounds/kick-bass.mp3')
+      let kick = new Audio('sounds/kick-bass.mp3');
       kick.play();
       break;
 
     default:
-      console.log(key)
+      console.log(key);
 
   }
-
-};
+}
 
 function buttonAnimation(currentKey) {
-  var activeButton = document.querySelector("." + currentKey);
+  let activeButton = $(`.${currentKey}`);
 
-  activeButton.classList.add("pressed");
-  activeButton.style.color = "#fff";
+  activeButton.addClass('pressed');
+  activeButton.css("color", "#fff");
 
   setTimeout(function() {
-    activeButton.classList.remove("pressed");
-    activeButton.style.color = "#DA0463"
+    activeButton.removeClass("pressed");
+    activeButton.css("color", "#DA0463");
   }, 150);
 }
